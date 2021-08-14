a27ca3dba23eab512c6021f892f207d0608365ad
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-08-14 20:28:18 +0900

    Initial commit

---
+++ src/main/java/net/kunmc/lab/rememberrecipequiz/RememberRecipeQuiz.java
@@ -0,0 +1,19 @@
+package net.kunmc.lab.rememberrecipequiz;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RememberRecipeQuiz extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}

e778a11b73b83f09bb8f1c13d86ea231586ca69b
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-08-15 15:13:05 +0900

    revert: rm gui

--- src/main/java/net/kunmc/lab/rememberrecipequiz/RememberRecipeQuiz.java
+++ src/main/java/net/kunmc/lab/rememberrecipequiz/RememberRecipeQuiz.java
@@ -1,6 +1,5 @@
 package net.kunmc.lab.rememberrecipequiz;
 
-import net.kunmc.lab.rememberrecipequiz.gui.GUIManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,7 +7,6 @@ public final class RememberRecipeQuiz extends JavaPlugin
 {
     public static Game game;
     private static RememberRecipeQuiz plugin;
-    private static GUIManager gui;
 
     public static RememberRecipeQuiz getPlugin()
     {
@@ -22,10 +20,6 @@ public final class RememberRecipeQuiz extends JavaPlugin
         Bukkit.getOnlinePlayers().stream().parallel()
                 .filter(player -> player.hasPermission("req.play"))
                 .forEach(game::addPlayer);
-
-        gui = new GUIManager(this);
-
-
 
     }
 
